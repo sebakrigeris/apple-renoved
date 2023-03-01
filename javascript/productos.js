6d1450826ec37b4a163725dcf4c48a0975703cd9
Sebastian Krigeris <sebastiankri123@gmail.com>
2023-03-01 16:48:30 -0300

    three commit

---
+++ javascript/productos.js
@@ -0,0 +1,152 @@
+const productos = [
+    {
+        id: 1,
+        img: '13ProMax-1.png',
+        store: 'Apple SL - Store',
+        name: 'iPhone 13 Pro Max',
+        price: 1.499,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 2,
+        img: '13ProMax-2.png',
+        store: 'Apple SL - Store',
+        name: 'iPhone 13 Pro Max' ,
+        price: 1.499,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 3,
+        img: '12Pro-1.png',
+        store: 'Apple SL - Store',
+        name: 'iPhone 12 Pro',
+        price: 1.199,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 4,
+        img: '12Pro-2.png',
+        store: 'Apple SL - Store',
+        name: 'iPhone 12 Pro',
+        price: 1.199,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 5,
+        img: '12Pro-3.png',
+        store: 'Apple SL - Store',
+        name: 'iPhone 12 Pro',
+        price: 1.199,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 6,
+        img: 'iPhone13-1.png',
+        store: 'Apple SL - Store',
+        name: 'iPhone 13',
+        price: 1.099,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 7,
+        img: 'iPhone13-2.png',
+        store: 'Apple SL - Store',
+        name: 'iPhone 13',
+        price: 1.099,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 8,
+        img: 'iPhone13-3.png',
+        store: 'Apple SL - Store',
+        name: 'iPhone 13',
+        price: 1.099,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 9,
+        img: 'iPhone13-4.png',
+        store: 'Apple SL - Store',
+        name: 'iPhone 13',
+        price: 1.099,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 10,
+        img: 'iPhone13-5.png',
+        store: 'Apple SL - Store',
+        name: 'iPhone 13',
+        price: 1.099,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 11,
+        img: 'Product-1.png',
+        store: 'AppleCase SL',
+        name: 'Case - iPhone 13',
+        price: 99,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 12,
+        img: 'Product-2.png',
+        store: 'AppleCase SL',
+        name: 'Case - iPhone 13',
+        price: 99,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 13,
+        img: 'Product-3.png',
+        store: 'AppleCase SL',
+        name: 'Case - iPhone 13',
+        price: 99,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 14,
+        img: 'Product-4.png',
+        store: 'AppleCase SL',
+        name: 'iPhone 13',
+        price: 99,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+
+    {
+        id: 15,
+        img: 'Product-5.png',
+        store: 'AppleCase SL',
+        name: 'iPhone 13',
+        price: 99,
+        text: '12 cuotas sin interes y envío a todo el país.',
+        quantity: 1,
+    },
+    
+];
